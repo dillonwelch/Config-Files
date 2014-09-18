26ec6a63253e21fe9c0514f8d06808068608d90e
Dillon Welch <daw0328@Dillons-MacBook-Air.local>
2014-09-17 17:13:31 -0700

    Actually adding install script.

---
+++ install_script.sh
@@ -0,0 +1,8 @@
+rm $HOME/.bashrc
+rm $HOME/.gitconfig
+rm $HOME/.gitignore
+rm $HOME/.vimrc
+ln -s .bashrc $HOME/.bashrc
+ln -s .gitconfig $HOME/.gitconfig
+ln -s .gitignore $HOME/.gitignore
+ln -s .vimrc $HOME/.vimrc
