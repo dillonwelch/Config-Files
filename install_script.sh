3af209fcb12d892785578eee5459c7b0212eb1c9
Dillon Welch <daw0328@gmail.com>
2017-02-06 13:26:57 -0800

    Improvements to install script

--- install_script.sh
+++ install_script.sh
@@ -3,8 +3,12 @@ rm $HOME/.gemrc
 rm $HOME/.gitconfig
 rm $HOME/.gitignore
 rm $HOME/.vimrc
+rm $HOME/.vim/vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/vundle.vim
 ln -s .bashrc $HOME/.bashrc
 ln -s .gemrc $HOME/.gemrc
 ln -s .gitconfig $HOME/.gitconfig
 ln -s .gitignore $HOME/.gitignore
 ln -s .vimrc $HOME/.vimrc
+ln -s .vim/vundle.vim $HOME/.vim/
+ln -s .vim/rename_current_file.vim $HOME/.vim/
