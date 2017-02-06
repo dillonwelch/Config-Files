22972772a387b522f35afca88064c3b373520c82
Dillon Welch <daw0328@gmail.com>
2017-02-06 13:21:34 -0800

    Add gemrc to install

--- install_script.sh
+++ install_script.sh
@@ -1,8 +1,10 @@
 rm $HOME/.bashrc
+rm $HOME/.gemrc
 rm $HOME/.gitconfig
 rm $HOME/.gitignore
 rm $HOME/.vimrc
 ln -s .bashrc $HOME/.bashrc
+ln -s .gemrc $HOME/.gemrc
 ln -s .gitconfig $HOME/.gitconfig
 ln -s .gitignore $HOME/.gitignore
 ln -s .vimrc $HOME/.vimrc
