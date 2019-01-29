6e1cf4c21fce9c8cae6d47e28e98725f70d1dec7
Dillon Welch <daw0328@gmail.com>
2019-01-29 13:56:39 -0500

    Add install for Homebrew + RVM

--- install_script.sh
+++ install_script.sh
@@ -13,3 +13,11 @@ ln -s $DIR/.gitignore $HOME/.gitignore
 ln -s $DIR/.vimrc $HOME/.vimrc
 ln -s $DIR/.vim/vundle.vim $HOME/.vim/
 ln -s $DIR/.vim/rename_current_file.vim $HOME/.vim/
+
+# Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install pinentry-mac gpg postgresql redis vim
+
+# RVM
+gpg --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+\curl -sSL https://get.rvm.io | bash -s stable
