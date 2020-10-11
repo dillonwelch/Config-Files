f12aec225c7057fc8e0865ea796612fa2d2e84e2
Dillon Welch <daw0328@gmail.com>
2020-10-10 22:04:35 -0400

    Add bash config + disable GPG for now

--- install_script.sh
+++ install_script.sh
@@ -35,3 +35,6 @@ gpg --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D695
 curl -sSL https://rvm.io/mpapis.asc | gpg --import -
 curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
 \curl -sSL https://get.rvm.io | bash -s stable
+
+# set Bash to the default shell
+chsh -s /bin/bash
